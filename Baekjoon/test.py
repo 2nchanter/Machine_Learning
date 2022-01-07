0fea8d0bfb57d12e32fc332d5353285509c03783
2nchanter <2nchanter@gmail.com>
2022-01-07 17:46:01 +0900

    END 1차원 배열

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,6 +1,5 @@
 import sys
-a = int(sys.stdin.readline().strip())
-n = 0
-b = (a//10)+(a%10)
-c = (a%10*10)+(b%10)
-print(c)
+# for i in sys.stdin.readline():
+#     print(i)
+for i in sys.stdin:
+    print(i)
--- Baekjoon/test2.py
+++ Baekjoon/test2.py
@@ -1,12 +1,3 @@
 import sys
-A = sys.stdin.readline().strip()
-a = int(A)
-n = 0
-while True:
-    n += 1
-    b = (a//10)+(a%10)
-    c = (a%10*10)+(b%10)
-    if int(A) == c:
-        print(n)
-        break
-    a = c
+for i in sys.stdin.readline():
+    print(i)
